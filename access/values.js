31b8cef3e0c823daf1ba9b354a1a09ada034bf0e
wrongzxc <131391793+wrongzxc@users.noreply.github.com>
2023-04-20 20:01:56 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "24.07.2004"; // Дата рождения
+var fullname = "Бабін Даниїл Сергійович" // ФИО
+var name = "Даниїл" // Имя
+
+// Паспорт
+var passport_id = "9563568521" // Номер паспорта
+
+// КПП
+var kpp_id = "9968654289" // ИНН
+
+// Права
+var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2025" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
